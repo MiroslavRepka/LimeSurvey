fe85f10cecd441926905982a420ac5ab8dc2d12c
Patricia Stelling <patricia.stelling@limesurvey.org>
2021-06-15 12:42:41 +0200

    dev: changes for modal views

--- application/views/layouts/partial_modals/modal_footer_canceldelete.php
+++ application/views/layouts/partial_modals/modal_footer_canceldelete.php
@@ -3,12 +3,10 @@
 
 <div class="modal-footer modal-footer-buttons">
     <button type="button" class="btn btn-cancel" data-dismiss="modal">
-        &nbsp
         <?php
         eT("Cancel"); ?>
     </button>
     <a class="btn btn-danger btn-ok">
-        &nbsp;
         <?php
         eT("Delete"); ?>
     </a>
