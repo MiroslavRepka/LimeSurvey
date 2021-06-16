0dc97f655a9bc4fb2fa039d24d80008f0abe6ffa
Patricia Stelling <patricia.stelling@limesurvey.org>
2021-06-16 09:24:06 +0200

    dev: changes for modal views

--- application/extensions/admin/grid/MassiveActionsWidget/views/modals/cancel-apply.php
+++ application/extensions/admin/grid/MassiveActionsWidget/views/modals/cancel-apply.php
@@ -34,7 +34,7 @@ echo $key; ?>"
 >
     <div class="modal-dialog <?php echo $largeModalView?>">
         <!-- Modal content-->
-        <div class="modal-content" style="color:#000">
+        <div class="modal-content" style="text-align:left; color:#000">
             <?php
             Yii::app()->getController()->renderPartial(
                 '/layouts/partial_modals/modal_header',
