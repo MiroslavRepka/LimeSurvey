fd57dc14d91fafdebb1bb4fd1f0ced0541d3aa8b
Miroslav Repka <miroslav.repka@berops.com>
2021-06-23 08:47:10 +0200

    Test if the DB credetials were incorrect

--- tests/functional/backend/InstallationControllerTest.php
+++ tests/functional/backend/InstallationControllerTest.php
@@ -61,7 +61,7 @@ class InstallationControllerTest extends TestBaseClassWeb
         }
         $dbpwd = getenv('DBPASSWORD');
         if (!$dbpwd) {
-            $dbpwd = '';
+            $dbpwd = 'root';
             echo 'Default to empty database password. Use DBPASSWORD=... from command-line to override this.' . PHP_EOL;
         }
 
